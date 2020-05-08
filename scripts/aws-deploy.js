5c96a3d15774295175d97be6a3725b6ec83c7d40
Jess <jesskim2613@gmail.com>
2020-05-07 17:39:54 -0700

    add env variable for credential

--- scripts/aws-deploy.js
+++ scripts/aws-deploy.js
@@ -2,6 +2,12 @@ const cleanBucket = require('./clean-s3bucket');
 const uploadBucket = require('./upload-s3bucket');
 const invalidate = require('./invalidate-cdn');
 const config = require('./config');
+var AWS = require('aws-sdk'); 
+
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+AWS.config.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+AWS.config.region = process.env.AWS_DEFAULT_REGION;
 
 async function deployDashboard(){
     try{
